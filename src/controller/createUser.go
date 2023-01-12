a4a3baefe8508508391e06e18111a2832be33652
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-01-12 13:52:02 -0300

    improve validation

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jjeanjacques10/crud-golang/src/configuration/rest_err"
+	"github.com/jjeanjacques10/crud-golang/src/configuration/validation"
 	"github.com/jjeanjacques10/crud-golang/src/controller/model/request"
 )
 
@@ -13,8 +13,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect field, error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
